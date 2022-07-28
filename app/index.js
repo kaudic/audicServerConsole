8ddd6070606797da6607e014c7ad3e7aefde02d8
Killian Audic <killianaudic@aol.fr>
2022-07-28 16:33:46 +0200

    trying to forward answer

--- app/index.js
+++ app/index.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.set('views', `${process.cwd()}/app/views`);
 app.set('view engine', 'ejs');
-app.use(express.static('./public'));
-// app.use(express.static('../imagesApp/public'));
+// app.use(express.static('./public'));
+app.use(express.static('../imagesApp/public'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

a403f923cf98ccbcfe0236073e1c5d802a4f1139
Killian Audic <killianaudic@aol.fr>
2022-07-28 12:08:26 +0200

    tracking proxy request

--- app/router.js
+++ app/router.js
@@ -11,8 +11,10 @@ const proxy = httpProxy.createProxyServer({});
 // Route for welcoming page - controling token first
 router.get('/', auth, controller.homePage);
 router.get('/imagesApp', (req, res) => {
+    console.log('coucou');
     // it will forward the request to imagesApp which is running on audicserver on port 4000 - ip below is the audicserver computer
     proxy.web(req, res, { target: 'http://192.168.1.18:4000' });
+    console.log('coucou2');
 });
 
 

f04f2f5f7ed2a5014277d0b9f4428a59e392b3b3
Killian Audic <killianaudic@aol.fr>
2022-08-02 14:48:16 +0200

    app with session ok

--- app/controller.js
+++ app/controller.js
@@ -12,25 +12,16 @@ const controller = {
 
         // check if info are correct
         if (login === process.env.LOGIN && password === process.env.PASSWORD) {
-
             // if yes then register user info in session
             req.session.user = {
                 login, password
             };
-
-            console.log('i will render now console.ejs');
-
             return res.json({
                 result: true,
                 message: 'Les informations de connexion ne sont pas correctes'
             });
 
-            // return res.render('console.ejs');
-
         } else {
-
-            console.log('i will tell that credentials are not corrects')
-
             return res.json({
                 result: false,
                 message: 'Les informations de connexion ne sont pas correctes'

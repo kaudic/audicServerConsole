d49becd22ea39d3200c51627dde195409220bb18
Killian Audic <killianaudic@aol.fr>
2022-09-21 16:39:28 +0200

    change in recipesAppBackRouter

--- app/routes/recipesAppBack.js
+++ app/routes/recipesAppBack.js
@@ -18,9 +18,9 @@ router.use('/', (req, res) => {
     // forward requests to target app
     if (process.env.NODE_ENV === 'production') {
         console.log('Request received in Proxy Prod for recipesAppBack');
-        proxy.web(req, res, { target: 'http://192.168.1.18:3001', selfHandleResponse: true, ws: true });
+        proxy.web(req, res, { target: 'http://192.168.1.18:3001' });
     } else {
-        proxy.web(req, res, { target: 'http://localhost:3001', selfHandleResponse: true, ws: true });
+        proxy.web(req, res, { target: 'http://localhost:3001' });
     }
 });
 
@@ -28,25 +28,6 @@ router.use('/', (req, res) => {
 proxy.on('error', (err, req, res) => {
     console.log('got an error : ', err)
 });
-
-// proxy.on('proxyRes', async (proxyRes, req, res) => {
-//     var body = [];
-//     proxyRes.on('data', function (chunk) {
-//         body.push(chunk);
-//     });
-//     proxyRes.on('end', function () {
-//         body = Buffer.concat(body).toString(); // sending string build from Buffer
-//         res.end(body);
-//     });
-// });
-
-// Listen to the `upgrade` event and proxy the
-// WebSocket requests as well.
-//
-// proxy.on('upgrade', function (req, socket, head) {
-//     console.log('upgrade en cours');
-//     proxy.ws(req, socket);
-// });
 
 
 module.exports = router;

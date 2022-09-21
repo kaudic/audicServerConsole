638ab9c25a7957d4aed8585cad22c6c174fbd0b5
Killian Audic <killianaudic@aol.fr>
2022-09-21 14:54:28 +0200

    added routeurs for recipesApp front and back

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,10 +5,15 @@ const router = express.Router();
 // Importation des routeurs secondaires
 const serverConsole = require('./serverConsole');
 const imagesAppRouter = require('./imagesApp');
+const recipesAppFrontRouter = require('./recipesAppFront');
+const recipesAppBackRouter = require('./recipesAppBack');
 
 // Appel aux routeurs secondaires
 router.use('/', serverConsole);
 router.use('/imagesApp', imagesAppRouter);
+router.use('/recipesAppFront', recipesAppFrontRouter);
+router.use('/recipesAppBack', recipesAppBackRouter);
+
 // router.use('/socket.io', imagesAppRouter);
 
 
---
+++ app/routes/recipesAppBack.js
@@ -0,0 +1,51 @@
+// importation des packages express
+const express = require('express');
+const router = express.Router();
+
+// Imports of middlewares
+const auth = require('../middlewares/sessionAuth');
+
+// REVERSE PROXY 
+const httpProxy = require('http-proxy');
+
+// proxy for requests (all except images and we will send string)
+const proxy = httpProxy.createProxyServer({});
+
+router.use(auth);
+
+// All request starting with /recipesAppBack will be here
+router.use('/', (req, res) => {
+    // forward requests to target app
+    if (process.env.NODE_ENV === 'production') {
+        proxy.web(req, res, { target: 'http://192.168.1.18:3001', selfHandleResponse: true, ws: true });
+    } else {
+        proxy.web(req, res, { target: 'http://localhost:3001', selfHandleResponse: true, ws: true });
+    }
+});
+
+// listenning to the proxy server events
+proxy.on('error', (err, req, res) => {
+    console.log('got an error : ', err)
+});
+
+// proxy.on('proxyRes', async (proxyRes, req, res) => {
+//     var body = [];
+//     proxyRes.on('data', function (chunk) {
+//         body.push(chunk);
+//     });
+//     proxyRes.on('end', function () {
+//         body = Buffer.concat(body).toString(); // sending string build from Buffer
+//         res.end(body);
+//     });
+// });
+
+// Listen to the `upgrade` event and proxy the
+// WebSocket requests as well.
+//
+// proxy.on('upgrade', function (req, socket, head) {
+//     console.log('upgrade en cours');
+//     proxy.ws(req, socket);
+// });
+
+
+module.exports = router;
